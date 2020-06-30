567ce2de8408142e050474bdc64315dabe26d871
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-06-30 19:44:48 -0400

    CREATED class Todo w/ handleChange, addTask methods. RENDER add newTask to the task list works.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Todo extends Component {
+  constructor () {
+    super();
+    this.state = {
+      tasks: [],
+      newTask: '',
+    }
+  };
 
-export default App;
+  handleChange = (e) => {
+    this.setState ({newTask: e.target.value})
+  };
+
+  addTask = (e) => {
+    let {tasks, newTask}=this.state;
+
+    e.preventDefault();
+    if(newTask !== '') {
+      // this.setState (prevState => {
+      //   return {tasks: prevState.tasks.concat(newTask)}
+      // })
+      
+      this.setState ({
+        newTask: '',
+        tasks: [...tasks, newTask]
+      })
+    }
+
+    console.log(tasks);
+  };
+
+  render () {
+    return (
+      <div className="container">
+        <form onSubmit={this.addTask}>
+          <label htmlFor="taskName" />
+          <input onChange={this.handleChange} name="taskName" type="text" placeholder= "Add a new task!"/>
+          <button type="submit">add</button>
+        </form>
+
+      </div>
+    )
+  };
+};
+
+export default Todo;
