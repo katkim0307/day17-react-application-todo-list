359e269ec5f0fab23f2b3cc5ddccbfd59fa8596a
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-06-30 23:39:28 -0400

    ADDED delete task button for each task in the array.

--- src/App.js
+++ src/App.js
@@ -31,7 +31,16 @@ class Todo extends Component {
     console.log(tasks);
   };
 
+  delTask = (idx) => {
+    const filteredTasks = this.state.tasks.filter((task, index) => {
+      return index !== idx;
+    });
+    console.log(filteredTasks);
+    this.setState ({tasks: filteredTasks});
+  };
+
   render () {
+    const {tasks} = this.state;
     return (
       <div className="container">
         <form onSubmit={this.addTask}>
@@ -39,7 +48,16 @@ class Todo extends Component {
           <input onChange={this.handleChange} name="taskName" type="text" placeholder= "Add a new task!"/>
           <button type="submit">add</button>
         </form>
-
+        <ul>
+          {tasks.map((item, idx) => {
+            return (
+              <li key={idx}>
+                {item}
+                <button type="button" onClick={() => this.delTask(idx)}>del</button>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     )
   };
